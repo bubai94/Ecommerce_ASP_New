073357ef17412578b87ad31ce76fee5568370419
Subhajit275 <subhajitsau@ibarts.in>
2026-05-30 02:58:34 +0530

    Create Admin Table Using MSSQL

--- Ecommerce/Models/dbstruc.cs
+++ Ecommerce/Models/dbstruc.cs
@@ -1,6 +1,14 @@
-﻿namespace Ecommerce.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Models
 {
-    public class dbstruc
+    public class dbstruc :DbContext
     {
+        public dbstruc(DbContextOptions<dbstruc> options) : base(options)
+        {
+        }
+        
+        public DbSet<Admin> tbl_Admin {  get; set; }
+
     }
 }
